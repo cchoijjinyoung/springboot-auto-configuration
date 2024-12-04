6e5dd7e990d8940b95299c7c4f30fdf704c279d0
cchoijjinyoung <cgy7127@gmail.com>
2024-12-05 01:00:31 +0900

    Begin a project

---
+++ src/main/java/com/toffee/springbootautoconfiguration/SpringbootAutoConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.toffee.springbootautoconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutoConfigurationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootAutoConfigurationApplication.class, args);
+  }
+
+}
